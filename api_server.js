22e1259fd1bbb9950ac3792c302cdefd700c92cd
Mac <>
2020-09-25 15:56:36 +0800

    登陆后台接口 接通完成

--- api_server.js
+++ api_server.js
@@ -1,26 +1,51 @@
 /**
  * 入口文件
  */
-
 //导入express模块
 const express = require('express')
 //创建 express 的服务器实例
 const app = express();
-
 //导入并配置cors 解决跨域问题
-const cors = require('cors')
+const cors = require('cors');
+//调用cors方法
 app.use(cors())
 
 //配置解析 application/x-www-form-urlencoded格式的表单数据的中间件
 app.use(express.urlencoded({
   extended: false
 }))
-
+//优化代码 将响应的数据中间件抽取
+app.use((req, res, next) => {
+  res.cc = (err, status = 1) => {
+    res.send({
+      //状态
+      status,
+      message: err instanceof Error ? err.message : err,
+    })
+  }
+  next();
+})
+//导入配置文件
+const config = require('./config');
+const expressJWT = require('express-jwt')
+//token身份认证
+app.use(expressJWT({
+  secret: config.jwtSecretKey
+}).unless({
+  path: [/^\/api\//]
+}))
 //导入并使用用户路由模块
 const user = require('./router/userAPI')
 app.use('/api', user)
-
-
+// 定义错误级别的中间件
+app.use((err, req, res, next) => {
+  // 验证失败导致的错误
+  // if (err instanceof joi.ValidationError) return res.cc(err)
+  // 身份认证失败后的错误
+  if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+  // 未知的错误
+  res.cc(err)
+})
 //启动服务器
 app.listen(80, () => {
   console.log('api server 127.0.0.1');
--- router_handler/user.js
+++ router_handler/user.js
@@ -1,14 +1,16 @@
 const db = require('../db/index')
+const bcrypt = require('bcryptjs')
+//导入生成Token的包
+const jwt = require('jsonwebtoken')
+//导入全局的配置文件
+const config = require('../config')
 exports.regUser = (req, res) => {
 
   // 获取客户端传输的数据
   const userinfo = req.body
   //判断用户名和密码是否为空
   if (!userinfo.username || !userinfo.password) {
-    return res.send({
-      status: 1,
-      message: '用户名密码不能为空'
-    })
+    return res.cc('用户名密码不能为空', 1)
   }
   //查询用户名是否被占用
   const sql = `select * from ev_users where username=?`
@@ -16,11 +18,11 @@ exports.regUser = (req, res) => {
     console.log(results);
     //检测用户名是否被占用
     if (results.length > 0) {
-      return res.send({
-        status: 1,
-        message: '用户名已占用'
-      })
+      return res.cc('用户名已占用', 1)
     }
+
+    //密码加密
+    userinfo.password = bcryptjs.hashSync(userinfo.password, 10);
     // 定义待执行的 SQL 语句
     const sqlStr = 'insert into ev_users set ?'
     db.query(sqlStr, {
@@ -31,13 +33,45 @@ exports.regUser = (req, res) => {
       // 执行 SQL 语句失败 
       if (err) return res.send(err.message)
       // 注册成功
-      res.send({
-        status: 0,
-        message: '注册成功！'
-      })
+      res.cc('注册成功!', 0);
     })
   })
 }
 exports.login = (req, res) => {
-  res.send('post list')
+  //登陆验证用户名、密码数据是否合法
+  // 获取客户端传输的数据
+  const userinfo = req.body
+  //判断用户名和密码是否为空
+  if (!userinfo.username || !userinfo.password) {
+    return res.cc('用户名密码不能为空', 1)
+  }
+  //查询用户名和密码是否在数据库中存在
+  const sql = `select * from ev_users where username=?`
+  db.query(sql, userinfo.username, (err, results) => {
+    if (err) {
+      return res.cc(err)
+    } else if (results.length !== 1) {
+      return res.cc('登陆失败')
+    }
+    //判断登陆密码和数据库密码是否一致
+    const compareResult = bcrypt.compareSync(userinfo.password, results[0].password);
+    if (!compareResult) {
+      return res.cc('登陆失败！')
+    }
+    //登陆成功，生成token字符串
+    const user = {
+      ...results[0],
+      password: '',
+      user_pic: 's'
+    }
+    //对用户信息进行加密，生成Token字符串
+    const tokenStr = jwt.sign(user, config.jwtSecretKey, {
+      expiresIn: config.expiresIn
+    })
+    res.send({
+      status: 0,
+      message: "登陆成功！",
+      token: 'Bearer ' + tokenStr
+    })
+  })
 }
